3c0b90dc934f2481dd6b9d13a26f7c3c70109183
Lays's Tot <97948444+Ebanaka21@users.noreply.github.com>
2024-09-22 19:58:58 +0300

    начал

---
+++ bestino/assets/javascript/application.js
@@ -0,0 +1,6 @@
+console.log('ok');
+let nav = document.getElementById("nav"),
+    button = document.getElementById("open")
+    button.addEventListener("click", () => {
+      nav.classList.toggle('header__nav--active')
+    })
